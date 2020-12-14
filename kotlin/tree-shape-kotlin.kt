70e1f2335d0f0837ffd1c5d5adfa6aa124315cf6
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-12-14 12:23:32 +0900

    Add Tree shape code!!

--- kotlin/tree-shape-kotlin.kt
+++ kotlin/tree-shape-kotlin.kt
@@ -9,10 +9,10 @@
         sharp = "#"; val space = " ";
      var res = ""; for (i in 1..h) { //
     res +="${space.repeat(h-i)}$left${sharp
-    .repeat(2*i+1)}$right\n" }; return res}
-            fun printTree() { val
-         space=" ".repeat(width);var
-        res = "$space★\n$space/#\\\n"
+   .repeat(2*i+1)}$right\n" }; return res} //
+             fun printTree() { val
+          space=" ".repeat(width);var
+         res = "$space★\n$space/#\\\n"
        val root = "${space}III\n".repeat(
        2) ; for (i in 1..height) { val a = 
      (if (i==1) 1 else 0); res += Triangle(
