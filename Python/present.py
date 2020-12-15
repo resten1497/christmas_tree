d234bcf6def76e931429dc816c8108cddea821a1
Daehyun Cho <1phantasmas@korea.ac.kr>
2020-12-15 21:09:00 +0900

    refactor

--- Python/present.py
+++ Python/present.py
@@ -6,15 +6,12 @@ import numpy as np
 size = 30
 
 lst = np.array(["'"]*30 + ['♡', '♧'])
-print(np.random.choice(lst, size=10))
 
 for i in range(0, size):
     if i == 0:
         print(Fore.YELLOW + "★".center(size, " "))
-
     elif i % 2 == 0 and i > 0:
         print(Fore.GREEN + ''.join(np.random.choice(lst, size=i)).center(size, " "))
-
     else:
         print(Fore.RED + ("'"*(i+1)).center(size, " "))
 
