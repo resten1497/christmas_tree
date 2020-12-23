1559e3c46d14f0f43d828dc083ce1aebc0ee74ef
D1N0-pixel <dino@DESKTOP-J6KB3MB.localdomain>
2020-12-23 12:57:42 +0900

    Add C quine

---
+++ c/quine.c
@@ -0,0 +1,104 @@
+int                                                          *
+                                                          *******
+                                                           *****
+                                                          *** ***
+                                                           XMAS;
+                                                          char*a;
+                                                         #include\
+                                                        <stdio.h>BY
+                                                       #include/**/\
+                                                      <stdlib.h> D1N0
+                                                     char*b="*******";
+                                                    #include <string.h>
+                                                       main(){ char*
+                                                      c= "int%59c%c";
+                                                     char*d="%65s%2$c"
+                                                    ;char*e="%64s%2$c";
+                                                   a=(char*)malloc(9999)
+                                                  ;char*f="%61s %s";char*
+                                                 g="%2$c%59cXMAS;%2$c%58c"
+                                                ;char*h="char*a;%2$c%7$57c"
+                                                   ;strcpy(a,c);char* i=
+                                                  "#include%c%2$c%7$56c";
+                                                 char*j="<stdio.h>BY%2$c";
+                                                char*k="%7$55c#include/**/"
+                                               ;char*l="%9$c%2$c%7$54c<stdl"
+                                              ;char*m="ib.h> D1N0%2$c";char*n
+                                             ="%7$53cchar*b=%c%s%10$c";char*o=
+                                            ";%2$c%7$52c#include <string.";char
+                                               *p="h>%2$c%7$55cmain(){ cha";
+                                              char*q="r*%2$c%7$54cc= %10$c%s"
+                                             ;char*r="%10$c;%2$c%7$53cchar*d";
+                                            char*s="=%10$c%s%10$c%2$c%7$52c;c";
+                                           char*t="har*e=%10$c%s%10$c;%2$c";char
+                                          *u="%7$51ca=(char*)malloc(9999)";char*v
+                                         ="%2$c%7$50c;char*f=%10$c%s%10$c;c";char*
+                                        w="har*%2$c%7$49cg=%10$c%s%10$c%2$c%7$48c";
+                                           char*x=";char*h=%10$c%s%10$c";char*y=
+                                          "%2$c%7$51c;strcpy(a,c);char* i=";char*
+                                         z="%2$c%7$50c%10$c%s%10$c;%2$c%7$49ccha";
+                                        char*A="r*j=%10$c%s%10$c;%2$c%7$48c";char*B
+                                       ="char*k=%10$c%s%10$c%2$c%7$47c;char*l=%10$c"
+                                      ;char*C="%s%10$c%2$c%7$46c;char*m=%10$c%s%10$c"
+                                     ;char*D=";char*n%2$c%7$45c=%10$c%s%10$c;char*o=";
+                                    char*E="%2$c%7$44c%10$c%s%10$c;char%2$c%7$47c";char
+                                       *F="*p=%10$c%s%10$c;%2$c%7$46cchar*q=%10$c%s"
+                                      ;char*G="%10$c%2$c%7$45c;char*r=%10$c%s%10$c;";
+                                     char*H="%2$c%7$44cchar*s=%10$c%s%10$c;%2$c%7$43c"
+                                    ;char*I="char*t=%10$c%s%10$c;char%2$c%7$42c";char*J
+                                   ="*u=%10$c%s%10$c;char*v%2$c%7$41c=%10$c%s%10$c;char"
+                                  ;char*K="*%2$c%7$40cw=%10$c%s%10$c;%2$c%7$43cchar";char
+                                 *L="*x=%10$c%s%10$c;char*y=%2$c%7$42c%10$c%s%10$c;";char*
+                                M="char*%2$c%7$41cz=%10$c%s%10$c;%2$c%7$40cchar*A=%10$c%s";
+                                   char*N="%10$c;char*B%2$c%7$39c=%10$c%s%10$c%2$c";char
+                                  *O="%7$38c;char*C=%10$c%s%10$c%2$c%7$37c;char*D="; char
+                                 *P="%10$c%s%10$c;%2$c%7$36cchar*E=%10$c%s%10$c;char%2$c";
+                                char*Q="%7$39c*F=%10$c%s%10$c%2$c%7$38c;char*G=%10$c";char*
+                               R="%s%10$c;%2$c%7$37cchar*H=%10$c%s%10$c%2$c%7$36c;char";char
+                              *S="*I=%10$c%s%10$c;char*J%2$c%7$35c=%10$c%s%10$c%2$c%7$34c;c";
+                             char*T="har*K=%10$c%s%10$c;char%2$c%7$33c*L=%10$c%s%10$c;ch";char
+                            *U="ar*%2$c%7$32cM=%10$c%s%10$c;%2$c%7$35cchar*N=%10$c%s%10$c;cha";
+                               char*V="r%2$c%7$34c*O=%10$c%s%10$c; char%2$c%7$33c*P";char*W=
+                              "=%10$c%s%10$c;%2$c%7$32cchar*Q=%10$c%s%10$c;char*%2$c";char*X=
+                             "%7$31cR=%10$c%s%10$c;char%2$c%7$30c*S=%10$c%s%10$c;%2$c";char*Y=
+                            "%7$29cchar*T=%10$c%s%10$c;char%2$c%7$28c*U=%10$c%s%10$c;%2$c";char
+                           *Z="%7$31cchar*V=%10$c%s%10$c;char*W=%2$c%7$30c%10$c%s%10$c;char*X=";
+                          char*Me="%2$c%7$29c%10$c%s%10$c;char*Y=%2$c%7$28c%10$c%s%10$c;char%2$c"
+                         ;char*rr="%7$27c*Z=%10$c%s%10$c;%2$c%7$26cchar*Me=%10$c%s%10$c%2$c";char*
+                        y_="%7$25c;char*rr=%10$c%s%10$c;char*%2$c%7$24cy_=%10$c%s%10$c;%2$c%7$23c";
+                       char*Ch="char*Ch=%10$c%s%10$c;char*ri%2$c%7$26c=%10$c%s%10$c;char*st";char*ri
+                          ="=%2$c%7$25c%10$c%s%10$c;%2$c%7$24cchar*ma=%10$c%s%10$c;char";char*st=
+                         "%2$c%7$23c*s_=%10$c%s%10$c;%2$c%7$22cchar*an=%10$c%s%10$c;%2$c%7$21cch";
+                        char*ma="ar*d_=%10$c%s%10$c;%2$c%7$20cchar*ha=%10$c%s%10$c;%2$c%7$19c";char
+                       *s_="char*ppy=%10$c%s%10$c;char%2$c%7$22c*ne=%10$c%s%10$c;%2$c%7$21cchar*w_";
+                      char*an="=%10$c%s%10$c;%2$c%7$20cchar*ye=%10$c%s%10$c;%2$c%7$19cchar*ar=%10$c";
+                     char*d_="%s%10$c;%2$c%7$18cchar*_1=%10$c%s%10$c;%2$c%7$17cchar*_2=%10$c%s%10$c;";
+                    char*ha="char*_3=%2$c%7$16c%10$c%s%10$c;char*%2$c%7$15c_4=%10$c%s%10$c;%2$c%7$14c";
+                   char*ppy="char*_5=%10$c%s%10$c;%2$c%7$13cchar*_6=%10$c%s%10$c;%2$c%7$12cchar*_7";char
+                      *ne="=%10$c%s%10$c;%2$c%s%85$s%85$s%85$s%85$s%85$s%85$s%s%86$s%86$s  a,d),e),";
+                     char*w_="f),g),h),i),j),k),l),m),n),o),p),q),r),s),t),u),v),w),x),y),z),A),B),C";
+                    char*ye="),D),E),F),G),H),I),J),K),L),M),N),O),P),%2$c  Q),R),S),T),U),V),W),X),Y";
+                   char*ar="),Z),Me),rr),y_),Ch),ri),st),ma),s_),an),d_),ha),ppy),ne),w_),ye),ar),_1),";
+                  char*_1="_2),_3),_4),_5),_6),%2$c  _7);char*_8=%10$c%s%10$c;char*_9=%10$c%s%10$c;char";
+                 char*_2="*_A=%10$c%00s%10$c;char _B[23], _C[123], _D[75];sprintf(_B,%10$c%%s%%s";char*_3=
+                "%%s%10$c,_9,_9,_9);sprintf(%2$c  _D,%10$c%%72s%%c%10$c,_B,10); sprintf(_C,%10$c  %%";char*
+               _4="s%%s%%s%%s%%s%%s%%s%%c%10$c,_B,_B,_B,_B,_B,_9,_9,10);printf(a,42,10,b,_A,_8,_8,32,32,92";
+              char*_5=",34,b,c,%2$c  d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,";
+             char*_6="M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,Me,rr,y_,Ch,ri,st,ma,%2$c  s_,an,d_,ha,ppy,ne,w_,ye,ar,_1,";
+            char*_7="_2,_3,_4,_5,_6,_7,_D,_C,_8,_9,_A); free(a); return 0; /* Tree Shaped Quine Code in C */}";
+                                                   strcat(strcat(strcat(
+                                                   strcat(strcat(strcat(
+                                                   strcat(strcat(strcat(
+                                                   strcat(strcat(strcat(
+                                                   strcat(strcat(strcat(
+                                                   strcat(strcat(strcat(
+                                                   strcat(strcat(strcat(
+  strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(
+  strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(
+  strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(strcat(
+  a,d),e),f),g),h),i),j),k),l),m),n),o),p),q),r),s),t),u),v),w),x),y),z),A),B),C),D),E),F),G),H),I),J),K),L),M),N),O),P),
+  Q),R),S),T),U),V),W),X),Y),Z),Me),rr),y_),Ch),ri),st),ma),s_),an),d_),ha),ppy),ne),w_),ye),ar),_1),_2),_3),_4),_5),_6),
+  _7);char*_8="***";char*_9="strcat(";char*_A="*****";char _B[23], _C[123], _D[75];sprintf(_B,"%s%s%s",_9,_9,_9);sprintf(
+  _D,"%72s%c",_B,10); sprintf(_C,"  %s%s%s%s%s%s%s%c",_B,_B,_B,_B,_B,_9,_9,10);printf(a,42,10,b,_A,_8,_8,32,32,92,34,b,c,
+  d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,Me,rr,y_,Ch,ri,st,ma,
+  s_,an,d_,ha,ppy,ne,w_,ye,ar,_1,_2,_3,_4,_5,_6,_7,_D,_C,_8,_9,_A); free(a); return 0; /* Tree Shaped Quine Code in C */}
