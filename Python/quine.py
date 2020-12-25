327898b04c9f51bd3676cf32b52943f1980abc25
D1N0-pixel <shjanghd@gmail.com>
2020-12-25 18:58:03 +0900

    Add Python quine

---
+++ Python/quine.py
@@ -0,0 +1,15 @@
+###
+_='     '
+Me=     '###\n'#
+rr=     '_=%r\nMe=%12r'
+y_=     '#\nrr=%20r\ny_=%26r'#
+Ch=     '#\nCh=%34r\nri=%r\nst=%r#%c'
+ri='\nma=%r#by D1N0\ns_=%14r;print((Me+rr+y'
+st='_+Ch#\n+ri%s+st+ma+s_)%%(_,Me,rr,##\ny_,%sCh,'#⭐
+ma='ri,st,11088\n,ma%s,s_,_,#\n_,_%'#by D1N0
+s_=     's,\n_))';print((Me+rr+y_+Ch#
++ri     +st+ma+s_)%(_,Me,rr,##
+y_,     Ch,ri,st,11088
+,ma     ,s_,_,#
+_,_     ,
+_))
