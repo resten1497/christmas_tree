f547659a4d990a3527026a3eaab9f90398c828b7
franknoh <franknoh1218@naver.com>
2021-05-02 19:37:12 +0900

    Update stylegan2.py

--- Python/stylegan2.py
+++ Python/stylegan2.py
@@ -2,7 +2,7 @@
 !pip install ninja
 %cd /content/stylegan2-ada-pytorch/
 from google_drive_downloader import GoogleDriveDownloader as gdd
-gdd.download_file_from_google_drive(file_id='1Hwv9ohoMdHL_KIdyV5Ue5D37-K-fC3Bq', dest_path='/content/models/model.pkl')
+gdd.download_file_from_google_drive(file_id='1d34d-AEJjj4uPTybbpz8c2u01oTo0B_6', dest_path='/content/models/model.pkl')
 !python generate.py --outdir=out --trunc=0.45 --seeds=0 \
     --network=/content/models/model.pkl
 from IPython.display import Image, display
