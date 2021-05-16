1c23547fa84179b17390d3760fa7d69f6784a65b
navier <69755061+navierr@users.noreply.github.com>
2021-05-16 21:47:52 +0900

    Fix typo

--- Haskell/tree.hs
+++ Haskell/tree.hs
@@ -7,4 +7,4 @@ main :: IO ()
 main = do
     putStrLn $ replicate (n - 1) ' ' ++ "*"
     mapM_ putStrLn [replicate (n - i) ' ' ++ replicate (2 * i - 1) 'A' | i <- [1..n]] 
-    mapM_ putStrLn $ replicate 3 $ replicate (n - 2) ' ' ++ "| |")
+    mapM_ putStrLn $ replicate 3 $ replicate (n - 2) ' ' ++ "| |"
