38220a472e643352b85445ce36403e38fb74cb08
navier <69755061+navierr@users.noreply.github.com>
2021-05-16 21:42:00 +0900

    Add: Haskell

---
+++ Haskell/tree.hs
@@ -0,0 +1,10 @@
+import Data.List
+
+n :: Int
+n = 9
+
+main :: IO ()
+main = do
+    putStrLn $ replicate (n - 1) ' ' ++ "*"
+    mapM_ putStrLn [replicate (n - i) ' ' ++ replicate (2 * i - 1) 'A' | i <- [1..n]] 
+    mapM_ putStrLn $ replicate 3 $ replicate (n - 2) ' ' ++ "| |")
